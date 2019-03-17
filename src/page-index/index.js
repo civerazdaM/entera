ff324d1242269489b6b60c5d1a39bba08df6de77
Stefan Madzarevic <stev291@gmail.com>
2019-03-17 21:08:45 +0100

    Adjusted design to new requirements

--- src/page-index/index.js
+++ src/page-index/index.js
@@ -1,1 +1,7 @@
 require("../main.css");
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faPhone } from "@fortawesome/free-solid-svg-icons/faPhone";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons/faEnvelope";
+
+library.add(faPhone, faEnvelope);
+dom.watch();
